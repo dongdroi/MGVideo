a3961f20be080b7d15eb031eea0ef7a16cee9b70
谈燕清 <tanyanqing@tanyanqingdeMacBook-Pro.local>
2016-05-19 14:42:32 +0800

    add media control

--- iOS/MGPlayerRCTManager.m
+++ iOS/MGPlayerRCTManager.m
@@ -17,10 +17,22 @@
 
 @property UIActivityIndicatorView *activityIndicator;
 @property(nonatomic) CGPoint center;
+
+@property(nonatomic,strong) UIView* myMediaView;
+@property(nonatomic,strong) UIButton *playButton;
+@property(nonatomic,strong) UIButton *pauseButton;
+@property(nonatomic,strong) UIButton *fullScreenButton;
+@property(nonatomic,strong) UILabel *currentTimeLabel;
+@property(nonatomic,strong) UILabel *totalDurationLabel;
+@property(nonatomic,strong) UISlider *mediaProgressSlider;
 @end
 
 @implementation MGPlayerRCTManager
+{
+  BOOL _isMediaSliderBeingDragged;
+}
 RCT_EXPORT_MODULE();
+
 
 - (UIView *)view
 {
@@ -28,18 +40,11 @@ RCT_EXPORT_MODULE();
   
   self.player = [MGMediaFactory createPlayer: [NSURL URLWithString:self.myurl] forUser:@"zhaohao" withSN:@"104" withKeyPath:nil];
   
+  [self initLoading];
+  [self initMedia];
   
-  self.activityIndicator =
-  [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 80.0f, 80.0f)];
-  [self.activityIndicator setCenter:self.center];
-  [self.activityIndicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhite];
-  [self.activityIndicator setHidesWhenStopped:YES];
-  
-  [self.player.view addSubview:self.activityIndicator];
-  
-  
-  [self.activityIndicator startAnimating];
   [self.player prepareToPlay];
+  
   
   return self.player.view;
 }
@@ -77,6 +82,235 @@ RCT_EXPORT_METHOD(stop)
 
 }
 
+-(void)initLoading
+{
+  self.activityIndicator =
+  [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 80.0f, 80.0f)];
+  [self.activityIndicator setCenter:self.center];
+  [self.activityIndicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhite];
+  [self.activityIndicator setHidesWhenStopped:YES];
+  [self.player.view addSubview:self.activityIndicator];
+  
+  [self.activityIndicator startAnimating];
+
+}
+
+-(void)initMedia
+{
+  self.myMediaView =[[UIView alloc]initWithFrame:CGRectMake(0.0, 0.0, self.center.x*2, self.center.y*2)];
+  self.myMediaView.userInteractionEnabled = YES;
+  [self.myMediaView addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(mediaViewTouchDown:)]];
+  
+  NSInteger gap = 5.0;
+  NSInteger viewHeight = 40;
+  
+  UIView *bottomView =
+  [[UIView alloc]initWithFrame:CGRectMake(0.0, self.center.y * 2 - viewHeight, self.center.x*2, viewHeight)];
+  [self.myMediaView addSubview:bottomView];
+  
+  
+  CGPoint startPoint = CGPointMake(gap,  bottomView.frame.size.height/2);
+  
+  
+  CGRect buttonRect = CGRectMake(startPoint.x, (viewHeight - 20)/2, 20.0f, 20.0f);
+  self.playButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  [self.playButton setFrame:buttonRect];
+  [self.playButton setImage:[UIImage imageNamed:@"ic_media_play"] forState:UIControlStateNormal];
+  [self.playButton setBackgroundImage:[UIImage imageNamed:@"ic_media_play"] forState:UIControlStateNormal];
+  [self.playButton addTarget:self action:@selector(onClickPlay:) forControlEvents:UIControlEventTouchUpInside];
+  [bottomView addSubview:self.playButton];
+  
+  self.pauseButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  [self.pauseButton setFrame:buttonRect];
+  [self.pauseButton setImage:[UIImage imageNamed:@"ic_media_pause"] forState:UIControlStateNormal];
+  [self.pauseButton setBackgroundImage:[UIImage imageNamed:@"ic_media_pause"] forState:UIControlStateNormal];
+  [self.pauseButton addTarget:self action:@selector(onClickPause:) forControlEvents:UIControlEventTouchUpInside];
+  [bottomView addSubview:self.pauseButton];
+  
+  startPoint.x += buttonRect.size.width + gap;
+  
+  
+  CGRect currentTimeRect = CGRectMake(startPoint.x, (viewHeight - 16)/2, 40.0f, 16.0f);
+  self.currentTimeLabel = [[UILabel alloc]initWithFrame:currentTimeRect];
+  self.currentTimeLabel.textColor = [UIColor whiteColor];
+  self.currentTimeLabel.text = @"--:--";
+  self.currentTimeLabel.numberOfLines = 1;
+  self.currentTimeLabel.adjustsFontSizeToFitWidth = YES;
+  self.currentTimeLabel.font = [UIFont boldSystemFontOfSize:10];
+  [bottomView addSubview:self.currentTimeLabel];
+  
+  startPoint.x += currentTimeRect.size.width + gap;
+  
+  CGRect sliderRect = CGRectMake(startPoint.x, (viewHeight - 20)/2, self.center.x*2 - startPoint.x - currentTimeRect.size.width - gap*2 - buttonRect.size.width, 20);
+  self.mediaProgressSlider = [[UISlider alloc]initWithFrame:sliderRect];
+  [self.mediaProgressSlider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+  [self.mediaProgressSlider addTarget:self action:@selector(beginDragMediaSlider:)  forControlEvents:UIControlEventTouchDown];
+  [self.mediaProgressSlider addTarget:self action:@selector(endDragMediaSlider:)  forControlEvents:UIControlEventTouchCancel];
+  [self.mediaProgressSlider addTarget:self action:@selector(didSliderTouchUpInside:)  forControlEvents:UIControlEventTouchUpInside];
+  [self.mediaProgressSlider addTarget:self action:@selector(didSliderTouchUpOutside:)  forControlEvents:UIControlEventTouchUpOutside];
+  [bottomView addSubview:self.mediaProgressSlider];
+  
+  startPoint.x += sliderRect.size.width + gap;
+  
+  
+  CGRect totalTimeRect = CGRectMake(startPoint.x, (viewHeight - 16)/2, 40.0f, 16.0f);
+  self.totalDurationLabel = [[UILabel alloc]initWithFrame:totalTimeRect];
+  self.totalDurationLabel.textColor = [UIColor whiteColor];
+  self.totalDurationLabel.text = @"--:--";
+  self.totalDurationLabel.numberOfLines = 1;
+  self.totalDurationLabel.adjustsFontSizeToFitWidth = YES;
+  self.totalDurationLabel.font = [UIFont boldSystemFontOfSize:10];
+  [bottomView addSubview:self.totalDurationLabel];
+  
+  startPoint.x += totalTimeRect.size.width + gap;
+  
+  
+  CGRect fullScreenRect = CGRectMake(startPoint.x, (viewHeight - 20)/2, buttonRect.size.width, buttonRect.size.height);
+  self.fullScreenButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  [self.fullScreenButton setFrame:fullScreenRect];
+  [self.fullScreenButton setImage:[UIImage imageNamed:@"ic_media_full"] forState:UIControlStateNormal];
+  [self.fullScreenButton setBackgroundImage:[UIImage imageNamed:@"ic_media_full"] forState:UIControlStateNormal];
+  [self.fullScreenButton addTarget:self action:@selector(onClickFullScreen:) forControlEvents:UIControlEventTouchUpInside];
+  [bottomView addSubview:self.fullScreenButton];
+  
+  [self.player.view addSubview:self.myMediaView];
+  
+  
+  self.player.view.userInteractionEnabled = YES;
+  [self.player.view addGestureRecognizer:[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(playerTouchDown:)]];
+
+  [self showAndFade];
+  [self performSelector:@selector(refreshMediaControl) withObject:nil afterDelay:0.5];
+}
+
+
+-(void)refreshMediaControl
+{
+  NSTimeInterval duration = self.player.duration;
+  NSInteger intDuration = duration + 0.5;
+  if (intDuration > 0) {
+    self.mediaProgressSlider.maximumValue = duration;
+    self.totalDurationLabel.text = [NSString stringWithFormat:@"%02d:%02d", (int)(intDuration / 60), (int)(intDuration % 60)];
+  }
+  else {
+    self.totalDurationLabel.text = @"--:--";
+    self.mediaProgressSlider.maximumValue = 1.0f;
+  }
+  
+  
+  
+  NSTimeInterval position;
+  if (_isMediaSliderBeingDragged && self.player.duration != 0) {
+    position = self.mediaProgressSlider.value;
+  } else {
+    position = self.player.currentPlaybackTime;
+  }
+  
+  NSInteger intPosition = position + 0.5;
+  if (intDuration > 0) {
+    self.mediaProgressSlider.value = position;
+  } else {
+    self.mediaProgressSlider.value = 0.0f;
+  }
+  self.currentTimeLabel.text = [NSString stringWithFormat:@"%02d:%02d", (int)(intPosition / 60), (int)(intPosition % 60)];
+  
+  if (self.myMediaView && !self.myMediaView.hidden)
+  {
+    [self performSelector:@selector(refreshMediaControl) withObject:nil afterDelay:0.5];
+  }
+  
+  
+  BOOL isPlaying = [self.player isPlaying];
+  self.playButton.hidden = isPlaying;
+  self.pauseButton.hidden = !isPlaying;
+}
+
+/**
+ *控制界面的显示和去除
+ */
+- (void) playerTouchDown:(id)sender
+{
+  [self showAndFade];
+}
+-(void) mediaViewTouchDown:(id)sender
+{
+  [self hide];
+}
+
+- (void)showNoFade
+{
+  self.myMediaView.hidden = NO;
+  [self cancelDelayedHide];
+  [self refreshMediaControl];
+}
+
+- (void)showAndFade
+{
+  [self showNoFade];
+  [self performSelector:@selector(hide) withObject:nil afterDelay:5];
+}
+
+- (void)hide
+{
+  self.myMediaView.hidden = YES;
+  [self cancelDelayedHide];
+}
+- (void)cancelDelayedHide {
+  [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(hide) object:nil];
+}
+
+/**
+ *控制界面的按钮
+ */
+- (void) onClickPlay:(id)sender
+{
+  if (self.player)
+  {
+    [self.player play];
+  }
+  
+}
+- (void) onClickPause:(id)sender
+{
+  if (self.player)
+  {
+    [self.player pause];
+  }
+}
+
+- (void) onClickFullScreen:(id)sender
+{
+}
+
+/**
+ *进度条相关监听
+ */
+- (void) sliderValueChanged:(id)sender
+{
+  [self refreshMediaControl];
+}
+- (void) beginDragMediaSlider:(id)sender
+{
+  _isMediaSliderBeingDragged = YES;
+}
+- (void) didSliderTouchUpInside:(id)sender
+{
+  [self.player setCurrentPlaybackTime:self.mediaProgressSlider.value];
+  _isMediaSliderBeingDragged = NO;
+}
+- (void) didSliderTouchUpOutside:(id)sender
+{
+  [self.player setCurrentPlaybackTime:self.mediaProgressSlider.value];
+  _isMediaSliderBeingDragged = NO;
+}
+
+- (void) endDragMediaSlider:(id)sender
+{
+  _isMediaSliderBeingDragged = NO;
+}
+
+
+
 /**
  *播放器的通知信息
  */
@@ -92,10 +326,10 @@ RCT_EXPORT_METHOD(stop)
 //                                               name:MGMPMoviePlayerPlaybackDidFinishNotification
 //                                             object:nil];
 //  
-//  [[NSNotificationCenter defaultCenter] addObserver:self
-//                                           selector:@selector(moviePlayBackStateDidChange:)
-//                                               name:MGMPMoviePlayerPlaybackStateDidChangeNotification
-//                                             object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(moviePlayBackStateDidChange:)
+                                               name:MGMPMoviePlayerPlaybackStateDidChangeNotification
+                                             object:nil];
 
 }
 
@@ -150,39 +384,39 @@ RCT_EXPORT_METHOD(stop)
 //}
 
 
-//- (void)moviePlayBackStateDidChange:(NSNotification*)notification {
-//  if (self.player != [notification object]) {
-//    return;
-//  }
-//  
-//  switch (self.player.playbackState) {
-//    case MGMPMoviePlaybackStateStopped: {
-//      NSLog(@"video state 正片状态变为: stoped");
-//      break;
-//    }
-//    case MGMPMoviePlaybackStatePlaying: {
-//      NSLog(@"video state 111正片状态变为: playing");
-//      break;
-//    }
-//    case MGMPMoviePlaybackStatePaused: {
-//      NSLog(@"video state 正片状态变为: paused");
-//      break;
-//    }
-//    case MGMPMoviePlaybackStateInterrupted: {
-//      NSLog(@"video state 正片状态变为: interrupted");
-//      break;
-//    }
-//    case MGMPMoviePlaybackStateSeekingForward:
-//    case MGMPMoviePlaybackStateSeekingBackward: {
-//      NSLog(@"video state 正片状态变为: seeking");
-//      break;
-//    }
-//    default: {
-//      NSLog(@"video state 正片状态变为未知 : %d", (int)_player.playbackState);
-//      break;
-//    }
-//  }
-//}
+- (void)moviePlayBackStateDidChange:(NSNotification*)notification {
+  if (self.player != [notification object]) {
+    return;
+  }
+  
+  switch (self.player.playbackState) {
+    case MGMPMoviePlaybackStateStopped: {
+      NSLog(@"video state 正片状态变为: stoped");
+      break;
+    }
+    case MGMPMoviePlaybackStatePlaying: {
+      NSLog(@"video state 111正片状态变为: playing");
+      break;
+    }
+    case MGMPMoviePlaybackStatePaused: {
+      NSLog(@"video state 正片状态变为: paused");
+      break;
+    }
+    case MGMPMoviePlaybackStateInterrupted: {
+      NSLog(@"video state 正片状态变为: interrupted");
+      break;
+    }
+    case MGMPMoviePlaybackStateSeekingForward:
+    case MGMPMoviePlaybackStateSeekingBackward: {
+      NSLog(@"video state 正片状态变为: seeking");
+      break;
+    }
+    default: {
+      NSLog(@"video state 正片状态变为未知 : %d", (int)_player.playbackState);
+      break;
+    }
+  }
+}
 
 
 
